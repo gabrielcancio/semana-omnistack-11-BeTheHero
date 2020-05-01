b8c1d1a11311873c10ffffcfe1019c52ff3c7315
gabrielcancio <gabriel.cancio02@gmail.com>
2020-05-01 19:05:57 -0300

    Connection to database finished

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,13 +1,39 @@
 const express = require('express');
+const crypto = require('crypto');
+const connection = require('./database/connection');
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-    return response.json({
-        evento: "Semana Omnnistack",
-        versão: 11.0,
-        aluno: "Gabriel Cancio"
+routes.get('/ongs', async(request, response) => {
+    const ongs = await connection('ongs').select('*');
+
+    return response.json(ongs);
+})
+
+routes.post('/ongs', async(request, response) => {
+    const { name, email, whatsapp, city, uf } = request.body;
+
+    const id = crypto.randomBytes(4).toString('HEX');
+
+    await connection('ongs').insert({
+        id,
+        name,
+        email,
+        whatsapp,
+        city,
+        uf,
     });
+
+    /*console.log({
+        id,
+        name,
+        email,
+        whatsapp,
+        city,
+        uf,
+    })*/
+
+    return response.json({ id });
 });
 
 module.exports = routes;
