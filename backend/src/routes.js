77dc298e5cbe2e32f762a19839ce2968bdc2b8a6
gabrielcancio <gabriel.cancio02@gmail.com>
2020-05-02 22:30:14 -0300

    Starting the controllers system

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,30 +1,20 @@
 const express = require('express');
-const crypto = require('crypto');
-const connection = require('./database/connection');
+
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
 
 const routes = express.Router();
 
-routes.get('/ongs', async(request, response) => {
-    const ongs = await connection('ongs').select('*');
+// Criar a rota para criar uma sessão (Login)
 
-    return response.json(ongs);
-})
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
 
-routes.post('/ongs', async(request, response) => {
-    const { name, email, whatsapp, city, uf } = request.body;
+routes.get('/profile', ProfileController.index);
 
-    const id = crypto.randomBytes(4).toString('HEX');
-
-    await connection('ongs').insert({
-        id,
-        name,
-        email,
-        whatsapp,
-        city,
-        uf,
-    });
-
-    return response.json({ id });
-});
+routes.post('/incidents', IncidentController.create);
+routes.get('/incidents', IncidentController.index);
+routes.delete('/incidents/:id', IncidentController.delete);
 
 module.exports = routes;
